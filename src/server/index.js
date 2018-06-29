48f6f18b798d18ed42abb037eea714e4241d014d
Vu Nguyen <tuanvu.vn007@gmail.com>
2018-06-29 15:37:20 +0700

    use react as server template envgine

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StaticRouter } from 'react-router-dom';
 import express from 'express';
 import { renderToString } from 'react-dom/server';
+import Document from './views/Document'
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
@@ -13,38 +14,17 @@ server
   .get('/*', (req, res) => {
     const context = {};
     const markup = renderToString(
-      <StaticRouter context={context} location={req.url}>
-        <MainApp />
-      </StaticRouter>
+      <Document assets={assets}>
+        <StaticRouter context={context} location={req.url}>
+          <MainApp />
+        </StaticRouter>
+      </Document>
     );
 
     if (context.url) {
       res.redirect(context.url);
     } else {
-      res.status(200).send(
-        `<!doctype html>
-          <html lang="">
-          <head>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            <meta charset="utf-8" />
-            <title>Welcome to Razzle</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            ${
-              assets.client.css
-                ? `<link rel="stylesheet" href="${assets.client.css}">`
-                : ''
-            }
-            ${
-              process.env.NODE_ENV === 'production'
-                ? `<script src="${assets.client.js}" defer></script>`
-                : `<script src="${assets.client.js}" defer crossorigin></script>`
-            }
-          </head>
-          <body>
-            <div id="root">${markup}</div>
-          </body>
-        </html>`
-      );
+      res.send("<!DOCTYPE html>" + markup);
     }
   });
 
---
+++ src/server/views/Document.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class Document extends Component {
+  render() {
+    const { children, assets } = this.props
+    return (
+      <html lang="">
+        <head>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta charset="utf-8" />
+          <title>Welcome to Razzle</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {assets.client.css && (
+            <link rel="stylesheet" href={assets.client.css} />
+          )}
+
+          {process.env.NODE_ENV === 'production' ? (
+            <script src={assets.client.js} defer />
+          ) : (
+            <script src={assets.client.js} defer crossorigin />
+          )}
+        </head>
+        <body>
+          <div id="root">{children}</div>
+        </body>
+      </html>
+    )
+  }
+}
+
+export default Document
